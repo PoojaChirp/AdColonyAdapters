b62d4b5a3b5eae08cb889ea8187e44adde11f3b9
PoojaChirp <pshashidhar@twitter.com>
2017-11-14 21:33:18 -0500

    Initial commit. Created an empty pod template using pod lib create

---
+++ Example/Pods/Target Support Files/AdColonyAdapters/AdColonyAdapters-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_AdColonyAdapters : NSObject
+@end
+@implementation PodsDummy_AdColonyAdapters
+@end
---
+++ Example/Pods/Target Support Files/Pods-AdColonyAdapters_Tests/Pods-AdColonyAdapters_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AdColonyAdapters_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_AdColonyAdapters_Tests
+@end
